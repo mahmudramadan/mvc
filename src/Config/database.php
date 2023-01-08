a8255b73a732a234d4498cf1cc0c31c5bc4ea8b2
mramadancorexc <mramadan@corexc.com>
2023-01-08 12:53:25 +0200

    update readme file

--- src/Config/database.php
+++ src/Config/database.php
@@ -7,7 +7,7 @@ $db = new DB;
 $db->addConnection([
     'driver' => 'mysql',
     'host' => 'localhost',
-    'database' => 'check24',
+    'database' => 'mvc',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
